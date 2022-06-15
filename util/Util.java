5aa1c19109277c1dc2b12f4a330ea32e609d07dc
FM-Moyano <94379185+FM-Moyano@users.noreply.github.com>
2022-06-15 15:48:15 +0200

    Add files via upload

--- util/Util.java
+++ util/Util.java
@@ -1,8 +1,11 @@
 package util;
 
+
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Util {
@@ -16,14 +19,14 @@ public class Util {
     System.out.print(prompt + ": ");
     return in.nextLine();
   }
-  
+
   public static int readInt(String prompt) {
     System.out.print(prompt + ": ");
     int n = in.nextInt();
     in.nextLine();  // sacamos \n del buffer de teclado
     return n;
   }
-   
+
   public static boolean isDateOk(String date) {
     try {
       LocalDate.parse(date, DateTimeFormatter.ofPattern("dd/MM/yyyy"));
@@ -32,7 +35,7 @@ public class Util {
       return false;
     }
   }
-  
+
   public static boolean esPrimo(int x) {
     for (int i = 2; i < x; i++) {
       if ((x % i) == 0) {
@@ -41,43 +44,80 @@ public class Util {
     }
     return true;
   }
-  
+
   public static void argumentoVacio(String[] args) {
     if(args.length == 0) {
       System.err.println("Error no tiene ningún argumento.");
       System.exit(1);
     }
   }
-  
- 
-  
+
+
+
   public static String readStr(String prompt, String end) {
     String str = readStr(prompt);
     System.out.print(end);
     return str;
   }
 
- 
-  
+
+
   public static int readInt(String prompt, String end) {
     int n = readInt(prompt);
     System.out.print(end);
     return n;
   }
-  
+
   public static double readDouble(String prompt) {
     System.out.print(prompt + ": ");
     double n = in.nextDouble();
     in.nextLine();  // sacamos \n del buffer de teclado
     return n;
   }
-  
+
   public static double readDouble(String prompt, String end) {
     double n = readDouble(prompt);
     System.out.print(end);
     return n;
   }
-  
+
+  public static double[] readIntArrayNotas(String prompt) {
+    List<Double> numeros = new ArrayList<>();
+
+    //int numero =  Util.readInt("Introduce numeros hasta introducir -1 ");
+    double numero =0;
+    
+    
+    /*while(numero != -1) {
+      numero = Util.readInt("Introduce numeros hasta introducir -1 ");
+           
+      if(numero >10) {
+        System.out.println("El numero debe estar entre 0 y 10");
+        numero = Util.readInt("Introduce numeros hasta introducir -1 ");
+      } else {
+        numeros.add(numero);
+      }
+      
+    }*/
+    
+    do {
+      if(numero >10 || numero < 0) {
+        System.out.println("Las notas debe estar entre 0 y 10");
+        //numero = Util.readInt("Introduce numeros hasta introducir -1 ");
+      } else {
+        numeros.add(numero);
+      }
+      numero = Util.readDouble("Introduce notas hasta introducir -1 ");
+    }while(numero!= -1);
+    
+    double[] numerosArray = new double[numeros.size()-1];
+
+    for(int i = 0 ; i < numeros.size()-1; i++) { 
+      numerosArray[i] = numeros.get(i+1);
+    }
+    return numerosArray;
+  }
+
   public static boolean confirm(String prompt) {
     String resp;
     for (;;) {
@@ -89,11 +129,11 @@ public class Util {
     }
     return resp.equals("S");
   }
-  
+
   public static boolean confirm() {
     return confirm("¿Está seguro/a? (S/N)");
   }
-  
+
   public static double read(String prompt, double defaultValue) {
     String returnValue = readStr(prompt + " [" + defaultValue + "]");
     return returnValue.isEmpty() ? defaultValue : Double.parseDouble(returnValue);
